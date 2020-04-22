91971accfb99312e4e03de58d8453c3dfaf7da42
Mohamed Hussein <mohamedhussein736@gmail.com>
2020-04-22 07:22:56 +0200

    Initial Commit

---
+++ src/measurement_package.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Eigen/Dense"
+
+class MeasurementPackage {
+public:
+  long timestamp_;
+
+  enum SensorType{
+    LASER,
+    RADAR
+  } sensor_type_;
+
+  Eigen::VectorXd raw_measurements_;
+
+};
+
